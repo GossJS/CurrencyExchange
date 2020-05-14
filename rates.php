d2ccb5e84deb815c6c0ebf17ca851a00ee5823bf
gossoudarev <goss@itmo.ru>
2020-05-14 12:29:39 +0300

    c4

--- rates.php
+++ rates.php
@@ -1,2 +1,7 @@
 <?php
- echo 10 * $_GET['dollars'];
+
+ $json = file_get_contents('https://kodaktor.ru/j/rates2');
+ $obj = json_decode($json);
+ $rate = $obj[0] -> sell;
+
+ echo $rate * $_GET['dollars'];
